71d42a182b579d4938d77bc9fdf60e2ee88e2ae3
honeyhyuni <tjfkqwkd001@naver.com>
2022-09-27 16:57:32 +0900

    added app/catube apps

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)  # CharField() -> 길이 제한있음
+    content = models.TextField()  # Text Filed() 는 길이 제한이없음
+
+
 # Create your models here.
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from app.models import Post
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    # qs = [{"id": 1, "title": "post 1"}, {"id": 2, "title": "post 2"}]
+    return render(
+        request,
+        "app/index.html",
+        {"post_list": qs},
+    )
+
 
 # Create your views here.
